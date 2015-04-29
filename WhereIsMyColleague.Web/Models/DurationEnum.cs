6ec84c92e57c13bf101be97681f61454ed5e30ee
Jonny <johnathon.broodie@nhs.net>
2015-04-29 14:54:06 +0100

    :hammer: Added enum template and display names

--- WhereIsMyColleague.Web/Models/DurationEnum.cs
+++ WhereIsMyColleague.Web/Models/DurationEnum.cs
@@ -1,7 +1,10 @@
 ﻿namespace WhereIsMyColleague.Web.Models
 {
+  using System.ComponentModel.DataAnnotations;
+
   public enum DurationEnum
   {
+    [Display(Name = "All Day")]
     AllDay,
     Morning,
     Afternoon
